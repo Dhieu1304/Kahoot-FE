c6638e0b352c5eeb6893a162682fa04eb5dce167
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-11-19 00:52:01 +0700

    [sang][UI][Add-Feature]: ManageGroupList: group-list

--- src/pages/GroupPage/components/ManageGroupList/GroupItem/GroupItem.js
+++ src/pages/GroupPage/components/ManageGroupList/GroupItem/GroupItem.js
@@ -1,5 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import Avatar from "../../../../../components/Avatar/Avatar";
+import styles from "./GroupItem.module.scss";
+
+const cx = classNames.bind(styles);
+
 function GroupItem() {
-   return <div>GroupItem</div>;
+   return (
+      <div className={cx("container")}>
+         <div className={cx("top")}>
+            <div className={cx("user-list")}>
+               <div className={cx("user-item")}>
+                  <Avatar
+                     src={
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1lU_Z3d6va3xVuzGdngT4mBWdhuEFrgNk6hngAsnw&s"
+                     }
+                     rounded
+                     size={25}
+                  />
+               </div>
+            </div>
+            <div className={cx("setting")}>
+               <FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" />
+            </div>
+         </div>
+
+         <div className={cx("bottom")}>
+            <span className={cx("group-name")}>Group 1</span>
+         </div>
+      </div>
+   );
 }
 
 export default GroupItem;
--- src/pages/GroupPage/components/ManageGroupList/ManageGroupList.js
+++ src/pages/GroupPage/components/ManageGroupList/ManageGroupList.js
@@ -1,11 +1,31 @@
+import classNames from "classnames/bind";
+
 import GroupItem from "./GroupItem";
 import TopBar from "./TopBar";
 
+import styles from "./ManageGroupList.module.scss";
+const cx = classNames.bind(styles);
+
 function ManageGroupList() {
    return (
-      <div>
+      <div className={cx("container")}>
          <TopBar />
-         <div>
+         <div className={cx("group-list")}>
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
+            <GroupItem />
             <GroupItem />
             <GroupItem />
             <GroupItem />
