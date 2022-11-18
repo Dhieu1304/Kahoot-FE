bdf9f1465b179ce2c0e02110699045e0569b4b67
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-11-19 01:11:38 +0700

    [sang][UI][Add-Feature] GroupPage: ManageGroupList: GroupItem: List user avatar

--- src/pages/GroupPage/components/ManageGroupList/GroupItem/GroupItem.js
+++ src/pages/GroupPage/components/ManageGroupList/GroupItem/GroupItem.js
@@ -5,20 +5,38 @@ import styles from "./GroupItem.module.scss";
 
 const cx = classNames.bind(styles);
 
+const users = [
+   {
+      avatar:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1lU_Z3d6va3xVuzGdngT4mBWdhuEFrgNk6hngAsnw&s"
+   },
+   {
+      avatar:
+         "https://toppng.com/uploads/preview/luffy-anime-one-piece-luffy-115628753714bjr6ypxar.png"
+   },
+   {
+      avatar:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBgF6F0tdXZ285Cn693WRLLF2iaDuq6pjextY4sEw-&s"
+   }
+];
+
 function GroupItem() {
    return (
       <div className={cx("container")}>
          <div className={cx("top")}>
             <div className={cx("user-list")}>
-               <div className={cx("user-item")}>
-                  <Avatar
-                     src={
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1lU_Z3d6va3xVuzGdngT4mBWdhuEFrgNk6hngAsnw&s"
-                     }
-                     rounded
-                     size={25}
-                  />
-               </div>
+               {users.map((user, index) => (
+                  <div
+                     key={index}
+                     className={cx("user-item")}
+                     style={{
+                        zIndex: users.length + 2 - index,
+                        left: index * 10 + 10
+                     }}
+                  >
+                     <Avatar src={user.avatar} rounded size={25} />
+                  </div>
+               ))}
             </div>
             <div className={cx("setting")}>
                <FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" />
