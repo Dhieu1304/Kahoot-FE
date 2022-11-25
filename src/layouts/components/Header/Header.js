7721b58e482363c1e4743814e99cb251a63ed644
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-11-25 20:54:50 +0700

    [sang][UI][Fix-bugs]: Header: Fix bugs responsive

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -15,7 +15,7 @@ import Avatar from "../../../components/Avatar/Avatar";
 
 import { headerItemsData } from "./config";
 import { useState } from "react";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, NavDropdown } from "react-bootstrap";
 import CustomToggleDropdownBtn from "../../../components/CustomToggleDropdownBtn";
 
 import * as localStorageApp from "../../../utils/localStorage";
@@ -29,77 +29,94 @@ function Header() {
 
    return (
       <header className={cx("wrapper")}>
-         <Container className={cx("container")}>
+         {/* <Container className={cx("container")}> */}
+         <Container>
             <Navbar bg="light" expand="lg">
-               <Navbar.Brand href="#home">
+               <Navbar.Brand as={Link} to={"/"}>
                   <div className={cx("logo-wrapper")}>
                      <img src={images.logo} alt="Kahoot" />
                   </div>
                </Navbar.Brand>
-               <Navbar.Toggle aria-controls="headerNavbar" />
-               <Navbar.Collapse id="headerNavbar">
-                  <Nav className="me-auto">
+               <Navbar.Toggle
+                  aria-controls="headerNavbar"
+                  className={cx("header-navbar-toggle-btn")}
+               />
+               <Navbar.Offcanvas id="headerNavbar" className={cx("header-navbar-off-canvas")}>
+                  <Nav className={cx("my-2 my-lg-0")} navbarScroll>
                      {headerItemsData.map((headerItem, index) => (
-                        <Nav.Item key={index}>
-                           <Nav.Link as={Link} to={headerItem.link}>
-                              <HeaderItem
-                                 title={headerItem.title}
-                                 active={headerItemActiveIndex === index}
-                                 leftIcon={headerItem.leftIcon}
-                                 onClick={() => setHeaderItemActiveIndex(index)}
-                              />
-                           </Nav.Link>
-                        </Nav.Item>
+                        <Nav.Link
+                           as={Link}
+                           to={headerItem.link}
+                           key={index}
+                           className={cx("navbar-left-link")}
+                        >
+                           <HeaderItem
+                              title={headerItem.title}
+                              active={headerItemActiveIndex === index}
+                              leftIcon={headerItem.leftIcon}
+                              onClick={() => setHeaderItemActiveIndex(index)}
+                           />
+                        </Nav.Link>
                      ))}
-                  </Nav>
-                  <Nav>
-                     <Nav.Item>
-                        <Nav.Link href="">
-                           <Button title={"Share"} outline outlineBlack rounded big />
-                        </Nav.Link>
-                     </Nav.Item>
-                     <Nav.Item>
-                        <Nav.Link href="">
-                           <Button title={"Create"} basic basicBlue rounded big />
-                        </Nav.Link>
-                     </Nav.Item>
-                     <Dropdown>
-                        <Dropdown.Toggle as={CustomToggleDropdownBtn} id="avatar-dropdown">
-                           <Avatar title={"Avatar"} placeholder={"Avatar"} size={25} rounded />
-                        </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                           <Dropdown.Item
-                              onClick={() => {
-                                 navigate("/profile");
-                              }}
-                           >
-                              Profile
-                           </Dropdown.Item>
-                           <Dropdown.Item
-                              onClick={() => {
-                                 localStorageApp.removeItem(
-                                    localStorageApp.LOCAL_STORAGE.ACCESS_TOKEN
-                                 );
-                                 localStorageApp.removeItem(
-                                    localStorageApp.LOCAL_STORAGE.REFRESH_TOKEN
-                                 );
-                                 window.location.reload(false);
-                              }}
-                           >
-                              Log out
-                           </Dropdown.Item>
-                        </Dropdown.Menu>
-                     </Dropdown>
-                     <Nav.Item className="d-flex align-items-center">
-                        <Nav.Link href="" className="d-flex align-items-center">
-                           <div className={cx("notify-icon-wrapper")}>
-                              <FontAwesomeIcon icon="fa-solid fa-bell" />
-                           </div>
-                        </Nav.Link>
-                     </Nav.Item>
+                     <div className={cx("header-navbar-off-canvas-right")}>
+                        <div className={cx("btn-group")}>
+                           <Button
+                              title={"Share"}
+                              outline
+                              outlineBlack
+                              rounded
+                              big
+                              className={cx("header-btn")}
+                           />
+                           <Button
+                              title={"Create"}
+                              basic
+                              basicBlue
+                              rounded
+                              big
+                              className={cx("header-btn")}
+                           />
+                        </div>
+                     </div>
                   </Nav>
-               </Navbar.Collapse>
+               </Navbar.Offcanvas>
+
+               <div className={cx("header-navbar-group-icons")}>
+                  <Dropdown className={cx("avatar-icon-container")}>
+                     <Dropdown.Toggle as={CustomToggleDropdownBtn} id="avatar-dropdown">
+                        <Avatar title={"Avatar"} placeholder={"Avatar"} size={25} rounded />
+                     </Dropdown.Toggle>
+
+                     <Dropdown.Menu>
+                        <Dropdown.Item
+                           className={cx("menu-item")}
+                           onClick={() => {
+                              navigate("/profile");
+                           }}
+                        >
+                           Profile
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                           className={cx("menu-item")}
+                           onClick={() => {
+                              localStorageApp.removeItem(
+                                 localStorageApp.LOCAL_STORAGE.ACCESS_TOKEN
+                              );
+                              localStorageApp.removeItem(
+                                 localStorageApp.LOCAL_STORAGE.REFRESH_TOKEN
+                              );
+                              window.location.reload(false);
+                           }}
+                        >
+                           Log out
+                        </Dropdown.Item>
+                     </Dropdown.Menu>
+                  </Dropdown>
+                  <div className={cx("notify-icon-container")}>
+                     <FontAwesomeIcon icon="fa-solid fa-bell" className={cx("notify-icon")} />
+                  </div>
+               </div>
             </Navbar>
          </Container>
       </header>
