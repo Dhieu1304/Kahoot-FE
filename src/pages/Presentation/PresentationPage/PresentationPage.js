0285b6828b0ed6936ab51d7ce4bca2bcca287da4
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-12-18 22:18:43 +0700

    [sang][UI][Feature][Presentation][list]: select row

--- src/pages/Presentation/PresentationPage/PresentationPage.js
+++ src/pages/Presentation/PresentationPage/PresentationPage.js
@@ -1,8 +1,9 @@
-import { faAdd, faPlayCircle, faX } from "@fortawesome/free-solid-svg-icons";
+import { faAdd, faL, faPlayCircle, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
 import { useEffect, useState } from "react";
 import { Container, Table } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
 import { Link } from "react-router-dom";
 
 import Button from "../../../components/Button";
@@ -16,6 +17,14 @@ function PresentationPage() {
 
    const [showCreateModal, setShowCreateModal] = useState(false);
 
+   const [isSelectAll, setIsSelectAll] = useState(false);
+   const [selectedRowIds, setSelectedRowIds] = useState([]);
+
+   const isDesktop = useMediaQuery({ minWidth: 992 });
+   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+   const isMobile = useMediaQuery({ maxWidth: 767 });
+   const isNotMobile = useMediaQuery({ minWidth: 768 });
+
    useEffect(() => {
       const loadData = async () => {
          presentationStore.method.loadPresentations();
@@ -24,8 +33,8 @@ function PresentationPage() {
       loadData();
    }, []);
 
-   console.log("presentationStore.state.presentations: ", presentationStore.state.presentations);
-   console.log("showCreateModal: ", showCreateModal);
+   // console.log("presentationStore.state.presentations: ", presentationStore.state.presentations);
+   // console.log("selectedRowIds: ", selectedRowIds);
 
    return (
       <div className={cx("wrapper")}>
@@ -48,35 +57,91 @@ function PresentationPage() {
                   </div>
                </div>
             </div>
-            <Table responsive className={cx("table")}>
-               <thead>
-                  <tr className={cx("tr")}>
-                     <th className={cx("th")}>Name</th>
-                     <th className={cx("th")}>Owner</th>
-                     <th className={cx("th")}>Code</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  {presentationStore.state.presentations?.map((presentation, index) => {
-                     return (
-                        <tr className={cx("tr")} key={index}>
-                           <td className={cx("td", "presentation-infor-col")}>
-                              <Link to={`${presentation.id}/1`}>
-                                 <FontAwesomeIcon
-                                    icon={faPlayCircle}
-                                    size="1x"
-                                    className={cx("icon")}
+
+            {isNotMobile ? (
+               <Table responsive className={cx("table")}>
+                  <thead>
+                     <tr className={cx("tr")}>
+                        <th className={cx("th")}>
+                           <input
+                              type={"checkbox"}
+                              checked={isSelectAll}
+                              onClick={() => {
+                                 if (isSelectAll) {
+                                    setSelectedRowIds([]);
+                                 } else {
+                                    setSelectedRowIds((prev) => {
+                                       const newSelectedRowIds =
+                                          presentationStore.state.presentations.map(
+                                             (presentation) => presentation.id
+                                          );
+                                       return [...newSelectedRowIds];
+                                    });
+                                 }
+
+                                 setIsSelectAll((prev) => !prev);
+                              }}
+                           />
+                        </th>
+                        <th className={cx("th")}>id</th>
+                        <th className={cx("th")}>Name</th>
+                        <th className={cx("th")}>Code</th>
+                        <th className={cx("th")}>Owner</th>
+                        <th className={cx("th")}>Modified</th>
+                        <th className={cx("th")}>Created</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {presentationStore.state.presentations?.map((presentation, index) => {
+                        const isChecked = selectedRowIds.includes(presentation.id);
+                        return (
+                           <tr className={cx("tr")} key={index}>
+                              <td className={cx("td")}>
+                                 <input
+                                    type={"checkbox"}
+                                    checked={isChecked}
+                                    onClick={() => {
+                                       setSelectedRowIds((prev) => {
+                                          const newSelectedRowIds = [...prev];
+                                          if (isChecked) {
+                                             const index = newSelectedRowIds.indexOf(
+                                                presentation.id
+                                             );
+                                             newSelectedRowIds.splice(index, 1);
+                                          } else {
+                                             newSelectedRowIds.push(presentation.id);
+                                          }
+                                          return [...newSelectedRowIds];
+                                       });
+                                       if (isChecked) {
+                                          setIsSelectAll(false);
+                                       }
+                                    }}
                                  />
-                              </Link>
-                              <Link to={`${presentation.id}/1/edit`}>{presentation?.name}</Link>
-                           </td>
-                           <td className={cx("td")}>Me</td>
-                           <td className={cx("td")}>{presentation?.code}</td>
-                        </tr>
-                     );
-                  })}
-               </tbody>
-            </Table>
+                              </td>
+                              <td className={cx("td")}>{presentation?.id}</td>
+                              <td className={cx("td", "presentation-infor-col")}>
+                                 <Link to={`${presentation.id}/1`}>
+                                    <FontAwesomeIcon
+                                       icon={faPlayCircle}
+                                       size="1x"
+                                       className={cx("icon")}
+                                    />
+                                 </Link>
+                                 <Link to={`${presentation.id}/1/edit`}>{presentation?.name}</Link>
+                              </td>
+                              <td className={cx("td")}>{presentation?.code}</td>
+                              <td className={cx("td")}>Me</td>
+                              <td className={cx("td")}>{presentation?.createdAt}</td>
+                              <td className={cx("td")}>{presentation?.updatedAt}</td>
+                           </tr>
+                        );
+                     })}
+                  </tbody>
+               </Table>
+            ) : (
+               <div></div>
+            )}
          </Container>
 
          <CreatePresentationModal show={showCreateModal} setShow={setShowCreateModal} />
