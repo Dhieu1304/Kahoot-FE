685bdb91a0e10e50d5adcdf9ae4afb7f7fca8765
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-01-03 00:16:29 +0700

    setup jwt socket

--- src/providers/socket/index.js
+++ src/providers/socket/index.js
@@ -1,5 +1,9 @@
 import socket_io from "socket.io-client";
 import { createContext } from "react";
+import { getItem, LOCAL_STORAGE } from "../../utils/localStorage";
 
-export const socket = socket_io.connect(process.env.REACT_APP_BE_URL);
+const token = getItem(LOCAL_STORAGE.ACCESS_TOKEN);
+
+export const socket = socket_io.connect(process.env.REACT_APP_BE_URL, { query: { token } });
+
 export const SocketContext = createContext({});
