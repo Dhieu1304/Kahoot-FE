bb463487c45fc76d9994535eef491ce459ec4ac0
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2023-01-11 02:16:44 +0700

    fix home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,21 @@
+import classNames from "classnames/bind";
+import styles from "./HomePage.module.scss";
+const cx = classNames.bind(styles);
+
 function HomePage() {
-   return <h1>HomePage</h1>;
+   return (
+      <div className={cx("wrapper")}>
+         <div className={cx("container")}>
+            <div className={cx("left")}>
+               <h1 className={cx("title")}>Engage your audience & eliminate awkward silences</h1>
+               <p className={cx("description")}>
+                  Our easy-to-build presentations, interactive Polls, Quizzes, and Word Clouds mean
+                  more participation and less stress
+               </p>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default HomePage;
